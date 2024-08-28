225f43011da238359003a05b1746d36d7f7db6ef
Guillermo Gallo <guillermogallo@Guillermos-MacBook-Pro.local>
2024-08-28 11:19:23 +0200

    add home text, style

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,19 @@
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+      <>
+        <div class="showcase-container">
+          <div class="header">
+              <h4>Welcome to my showcase website!</h4>
+          </div>
+          <div class="content">
+              <span>
+                  This is a web app built using single-spa framework. <br>
+                  Each section is a different micro-frontend. <br>
+                  Use the links in the sidebar to navigate through them! <br>
+                  Work in progress...
+              </span>
+          </div>
+        </div>
+      </>
+  );
 }
