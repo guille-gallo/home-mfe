24b166b4ef33799e56638ecc7c636787260ef939
Guillermo Gallo <guillermogallo@Guillermos-MacBook-Pro.local>
2024-08-28 11:40:22 +0200

    Updates

--- root.component.d.ts
+++ root.component.d.ts
@@ -1,2 +1,1 @@
-import './root.component.scss';
-export default function Root(props: any): import("react/jsx-runtime").JSX.Element;
+export default function Root(): import("react/jsx-runtime").JSX.Element;
